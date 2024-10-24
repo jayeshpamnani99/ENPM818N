d96d953daa2697f0fd0e2b03bd279a1e5545891a
Jayesh Pamnani <jayeshpamnani15@gmail.com>
2024-10-24 14:43:13 -0400

    modify connect.php

--- ecommerceAPP/includes/connect.php
+++ ecommerceAPP/includes/connect.php
@@ -1,23 +1,29 @@
 <?php
-
 // Enable error reporting
 error_reporting(-1);
 ini_set('display_errors', TRUE);
 
-// Define database connection variables (these will be set by UserData)
+// Define database connection variables
 $servername = '';
 $username = '';
 $password = '';
 $dbname = '';
 
-// Creating a new MySQLi connection
-$con = new mysqli($servername, $username, $password, $dbname);
+// Set the charset explicitly
+mysqli_set_charset($con, 'utf8mb4');
+
+// Creating a new MySQLi connection with charset specification
+$con = mysqli_init();
+mysqli_options($con, MYSQLI_OPT_CONNECT_TIMEOUT, 30);
+mysqli_real_connect($con, $servername, $username, $password, $dbname);
 
 // Check connection
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
 
-// Your application code goes here...
+// Set the charset after connection
+$con->set_charset("utf8mb4");
 
+// Your application code goes here...
 ?>
