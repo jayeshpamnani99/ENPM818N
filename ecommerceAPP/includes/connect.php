cbfd212a1dfc2729ad03ca26d11936ed6d42b6d0
Jayesh Pamnani <jayeshpamnani15@gmail.com>
2024-10-24 02:12:09 -0400

    modify connect.php

--- ecommerceAPP/includes/connect.php
+++ ecommerceAPP/includes/connect.php
@@ -1,37 +1,14 @@
 <?php
 
-require 'vendor/autoload.php'; 
-
-use Aws\SecretsManager\SecretsManagerClient;
-use Aws\Exception\AwsException;
-
 // Enable error reporting
 error_reporting(-1);
 ini_set('display_errors', TRUE);
 
-$client = new SecretsManagerClient([
-    'version' => 'latest',
-    'region' => 'us-east-1', 
-]);
-
-try {
-    $result = $client->getSecretValue([
-        'SecretId' => 'ecommerce/rds/credentials2', 
-    ]);
-    
-    // Decode the secret string into an associative array
-    $secret = json_decode($result['SecretString'], true);
-    
-    // Use retrieved credentials
-    $servername = $secret['RDS_HOST'];
-    $username = $secret['RDS_USER'];
-    $password = $secret['RDS_PASSWORD'];
-    $dbname = $secret['RDS_DBNAME'];
-    
-} catch (AwsException $e) {
-    // Error Handling in retrieving secret
-    die("Error retrieving secret: " . $e->getMessage());
-}
+// Define database connection variables (these will be set by UserData)
+$servername = '';
+$username = '';
+$password = '';
+$dbname = '';
 
 // Creating a new MySQLi connection
 $con = new mysqli($servername, $username, $password, $dbname);
